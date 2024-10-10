071c3cbdd658deaa7f0df66e62e8cc0061488497
Nicola Tarocco <ntarocco@gmail.com>
2024-10-10 22:41:15 +0200

    job: refactor args
    * make `since` a predefined argument
    * make extra args optional

--- invenio_jobs/jobs.py
+++ invenio_jobs/jobs.py
@@ -7,17 +7,23 @@
 
 """Jobs module."""
 
-from abc import ABC, abstractmethod
+from abc import ABC
+
+from marshmallow import fields
 
 
 class JobType(ABC):
-    """Base class to register celery tasks available in the admin panel."""
+    """Base class to define a job."""
 
-    arguments_schema = None
-    task = None
     id = None
     title = None
     description = None
+
+    task = None
+
+    arguments_schema = {
+        "since": fields.DateTime(required=False),
+    }
 
     @classmethod
     def create(
@@ -39,17 +45,29 @@ class JobType(ABC):
             ),
         )
 
-    @abstractmethod
-    def default_args(self, *args, **kwargs):
-        """Abstract method to enforce implementing default arguments."""
+    @classmethod
+    def get_task_args(cls, job_obj, **kwargs):
+        """Override to define extra arguments to be injected on task execution.
+
+        :param job_obj (Job): the Job object.
+        :return: a dict of arguments to be injected on task execution.
+        """
         return {}
 
     @classmethod
-    def build_task_arguments(cls, job_obj, custom_args=None, **kwargs):
-        """Build arguments to be passed to the task.
+    def build_task_arguments(cls, job_obj, since=None, custom_args=None, **kwargs):
+        """Build dict of arguments injected on task execution.
 
-        Custom arguments can be passed to overwrite the default arguments of a job.
+        :param job_obj (Job): the Job object.
+        :param since (datetime): last time the job was executed.
+        :param custom_args (dict): when provided, takes precedence over any other
+            provided argument.
+        :return: a dict of arguments to be injected on task execution.
         """
         if custom_args:
             return custom_args
-        return cls.default_args(job_obj, **kwargs)
+
+        # `since` is a predefined argument
+        if since is None and job_obj.last_runs["success"]:
+            since = job_obj.last_runs["success"].started_at
+        return {"since": since, **cls.get_task_args(job_obj, **kwargs)}
