49d6b7ccee19f3eec7362f4aa10f650de471e229
Sam Arbid <samaa2@kth.se>
2025-07-08 11:48:15 +0200

    i18n: test sv language

--- invenio_jobs/assets/semantic-ui/translations/invenio_jobs/messages/_generatedTranslations.js
+++ invenio_jobs/assets/semantic-ui/translations/invenio_jobs/messages/_generatedTranslations.js
@@ -2,7 +2,9 @@
 // This file exports all available translations for i18next
 
 import TRANSLATE_EN from "./en/translations.json";
+import TRANSLATE_SV from "./sv/translations.json";
 
 export const translations = {
   en: { translation: TRANSLATE_EN },
+  sv: { translation: TRANSLATE_SV },
 };
