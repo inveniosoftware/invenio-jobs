939deea38f54edf59552b574fe5e9a5653bbae91
Sam Arbid <samaa2@kth.se>
2025-07-08 14:53:17 +0200

    fix: run auto linter

--- invenio_jobs/assets/semantic-ui/js/invenio_jobs/administration/RunsLogsView.js
+++ invenio_jobs/assets/semantic-ui/js/invenio_jobs/administration/RunsLogsView.js
@@ -1,5 +1,6 @@
 // This file is part of Invenio
 // Copyright (C) 2024 CERN.
+// Copyright (C) 2025 KTH Royal Institute of Technology.
 //
 // Invenio RDM is free software; you can redistribute it and/or modify it
 // under the terms of the MIT License; see LICENSE file for more details.
@@ -17,11 +18,7 @@ if (detailsConfig) {
   const run = JSON.parse(detailsConfig.dataset.run);
   const sort = JSON.parse(detailsConfig.dataset.sort);
   ReactDOM.render(
-    <RunsLogs
-      logs={logs}
-      run={run}
-      sort={sort}
-    />,
+    <RunsLogs logs={logs} run={run} sort={sort} />,
     detailsConfig
   );
 }
