c544e23b68ee321c13112c5406c01e12a7843bd6
Maximilian Moser <maximilian.moser@tuwien.ac.at>
2026-07-16 16:08:45 +0200

    release: v11.0.1

--- invenio_jobs/__init__.py
+++ invenio_jobs/__init__.py
@@ -1,12 +1,13 @@
 # SPDX-FileCopyrightText: 2024-2026 CERN.
 # SPDX-FileCopyrightText: 2024-2026 Graz University of Technology.
 # SPDX-FileCopyrightText: 2025 KTH Royal Institute of Technology.
+# SPDX-FileCopyrightText: 2026 TU Wien.
 # SPDX-License-Identifier: MIT
 
 """InvenioRDM module for jobs management."""
 
 from .ext import InvenioJobs
 
-__version__ = "11.0.0"
+__version__ = "11.0.1"
 
 __all__ = ("__version__", "InvenioJobs")
